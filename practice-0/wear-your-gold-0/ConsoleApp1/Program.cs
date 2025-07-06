d3f2da8068146b8ab8a827903d44b6994c15c5cd
eighteightcruizer <219153516+eighteightcruizer@users.noreply.github.com>
2025-07-06 23:13:55 +0100

    Initial commit

---
+++ practice-0/wear-your-gold-0/ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Welcome back your recovery. ");
+Console.WriteLine("Today is the day!");
+Console.Write("I did set-up github, today!");
+// That's the comment 
